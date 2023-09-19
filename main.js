a828645d7ba87eaca6de90e4595c6d35095a584e
claritaesla <112556503+claritaesla@users.noreply.github.com>
2023-09-18 22:59:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-const apiUrl = 'https://cataas.com/'
+async function carregar_img(){
+    const url = 'https://cataas.com/cat'
 
-fetch(apiUrl)
-    
+    const options = {
+        method: "GET"
+    }
+
+    let response = await fetch(url, options)
+    if (response.status === 200) {
+        const imageCat = await response.blob()
+        const imageObjectURL = URL.createObjectURL(imageCat);
+
+        const image = document.createElement('img')
+        image.src = imageObjectURL
+
+        const container = document.getElementById('myDiv')
+        container.append(image)
+    }
+    else {
+        console.log("HTTP-ERROR:" + response.status)
+    }
+
+}
